e0fe8d6657f3c60b0f422d33c6f7b8fec9a8e830
Paulina <paulina.a.wojtal@gmail.com>
2021-04-14 09:54:22 +0200

    Directory A created and added 3 files and 1 directory to it.

---
+++ composite/src/main/java/org/example/composite/Directory.java
@@ -0,0 +1,46 @@
+package org.example.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory implements FileSystemElement{
+
+    private String name;
+    private List<FileSystemElement> dirElements = new ArrayList<>();
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean rename(String newName) {
+        this.name = newName;
+        return true;
+    }
+
+    @Override
+    public boolean remove() {
+        return true;
+    }
+
+    @Override
+    public boolean move() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void print() { //should iterate over all elements
+        dirElements.forEach(System.out::println);
+    }
+
+    @Override
+    public void add(FileSystemElement element) {
+        dirElements.add(element);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", name);
+    }
+}
---
+++ composite/src/main/java/org/example/composite/FileSystemElement.java
@@ -0,0 +1,12 @@
+package org.example.composite;
+
+public interface FileSystemElement {
+
+    public boolean rename(String newName);
+    public boolean remove();
+
+    //dodać ścieżkę docelową w parametrach
+    public boolean move();
+    public void print();
+    public void add(FileSystemElement element);
+}
